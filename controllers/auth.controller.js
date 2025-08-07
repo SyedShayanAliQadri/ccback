a666cea02119dba2bb2b5d202103fa9272b7e54c
Syed Shayan Ali Qadri <syedshayanaliqadri@gmail.com>
2025-08-07 17:23:37 +0500

    Update auth.controller.js

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -39,7 +39,8 @@ export const login = async (req, res, next) => {
     res
       .cookie("accessToken", token, {
         httpOnly: true,
-        
+        secure: true,
+        sameSite: "None",
       })
       .status(200)
       .send(info);
@@ -51,8 +52,9 @@ export const login = async (req, res, next) => {
 export const logout = async (req, res) => {
   res
     .clearCookie("accessToken", {
-      sameSite: "none",
+      httpOnly: true,
       secure: true,
+      sameSite: "None",
     })
     .status(200)
     .send("User has been logged out.");
